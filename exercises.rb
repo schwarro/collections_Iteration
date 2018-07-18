75d26cbfa2165135238e3c89ec0599b06daf9478
schwarro <rob.ap.schwartz@gmail.com>
2018-07-18 15:10:20 -0400

    completed exercise 0

---
+++ exercises.rb
@@ -0,0 +1,31 @@
+#exercise 0
+
+fav_colours = ["purple", "red", "blue", "black"]
+family_age = [59, 58, 30, 28, 23]
+coin_flip = ["heads", "heads", "tails", "heads", "tails"]
+fav_artists = ["The Beatles", "Rush", "Coldplay"]
+fav_colours = [:purple, :red, :blue, :black]
+
+definitions= {
+  :computer => "an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program.",
+  :dog => "a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, and a barking, howling, or whining voice. It is widely kept as a pet or for work or field sports.",
+  :couch => "a long upholstered piece of furniture for several people to sit on."
+}
+
+fav_movies= {
+  :"The Social Network" => "2010",
+  :"V for Vendetta" => "2006",
+  :"The Avengers" => "2012"
+}
+
+cities= {
+  :"Toronto" => "2.81 Million",
+  :"Montreal" => "1.74 Million",
+  :"Vancouver" => "0.65 Million"
+}
+
+ages= {
+  :"David" => 28,
+  :"Matthew" => 23,
+  :"Amanda" => 28
+}
