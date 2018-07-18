9b76077b4894eb29f41077e8fb7b190b1a524357
schwarro <rob.ap.schwartz@gmail.com>
2018-07-18 17:49:22 -0400

    completed exercise 11

---
+++ exercise10.rb
@@ -0,0 +1,13 @@
+str = Array(1..100)
+
+str.each do |x|
+  if (x % 3 == 0) && (x % 5 == 0)
+    puts "Bitmaker"
+  elsif x % 3 == 0
+    puts "Bit"
+  elsif x % 5 == 0
+    puts "Maker"
+  else
+    puts x
+  end
+end 
